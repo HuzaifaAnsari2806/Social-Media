f7845262415b0f5b0963bb7f5f1695b5d71f21d0
unknown <ansarihuzaifa2806@gmail.com>
2024-11-27 15:53:44 +0530

    added static root and media root

--- core/settings.py
+++ core/settings.py
@@ -163,6 +163,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "/media/"
+
+# Directory where media files will be stored
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
